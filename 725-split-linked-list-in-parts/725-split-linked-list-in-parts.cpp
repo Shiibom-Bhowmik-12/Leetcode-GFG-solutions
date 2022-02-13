40b669b7a12fe61e6c32811ca671459c38ca1207
Shiibom-Bhowmik-12 <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-02-13 06:09:11 +0530

    Time: 18 ms (11.79%), Space: 8.7 MB (91.91%) - LeetHub

--- 725-split-linked-list-in-parts/725-split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/725-split-linked-list-in-parts.cpp
@@ -12,16 +12,23 @@ class Solution {
 public:
     vector<ListNode*> splitListToParts(ListNode* head, int k) {
         vector<ListNode *> ans(k);
+        if(head==NULL or k==0){
+            return ans;
+        }
+        
         int total_length=0;
         
         ListNode *curr=head;
+        
+        //calculating the length of LL
         while(curr){
             total_length++;
             curr=curr->next;
         }
         
-        int len=total_length / k;
-        int extra_nodes=total_length % k;
+        
+        int len=total_length / k;//finding the length of each sublist
+        int extra_nodes=total_length % k;//finding the no. of extra nodes which                                            //have to be connected with each list
         
         curr=head;
         ListNode *prev=NULL;
@@ -30,16 +37,17 @@ public:
         while(curr!=NULL and i<k){
             ans[i]=curr;
             
+            //for the extra nodes we add it to all the list until the count of                 //extra nodes becomes 0.
             for(int j=0;j<len+(extra_nodes>0?1:0);j++){
                 prev=curr;
                 curr=curr->next;
             }
             
-            prev->next=NULL;
-            i++;
-            extra_nodes--;
+            prev->next=NULL;//making the nex tof each sublist as NULL
+            i++;//incrementing the count of no. of sublists
+            extra_nodes--;//decrementing the no. of extra nodes.
         }
         
-        return ans;
+        return ans;//returning the resultant vector containing all the sublists.
     }
 };
