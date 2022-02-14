1f089641a2425f9786189069559726f3e409420f
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-02-15 04:51:46 +0530

    Time: 8 ms (94.36%), Space: 11.5 MB (42.16%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -11,33 +11,77 @@
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
-        ListNode* new_head = new ListNode(0);
-        new_head->next = head;
-        ListNode* cur = new_head;
-        while(cur != NULL)
-        {
-            int sum=0;
+        // ListNode* new_head = new ListNode(0);
+        // new_head->next = head;
+//         ListNode* cur = new_head;
+//         while(cur != NULL)
+//         {
+//             int sum=0;
             
-            //calculating the sum
-            //if sum becomes 0 then we skip all the nodes so far traversed
-            while(head != NULL)
-            {
-                sum += head->val;
-                if(sum == 0)
-                {
-                   cur->next = head->next; 
+//             //calculating the sum
+//             //if sum becomes 0 then we skip all the nodes so far traversed
+//             while(head != NULL)
+//             {
+//                 sum += head->val;
+//                 if(sum == 0)
+//                 {
+//                    cur->next = head->next; 
+//                 }
+//                 head = head->next;
+//             }
+            
+//             //moving on to the next node for performing the above operation 
+//             //operation to check if there exists a sum that equals to zero
+//             cur = cur->next;
+//             if(cur != NULL)
+//             {
+//                 head = cur->next;
+//             }
+//         }
+//         return new_head->next; 
+//----------------         ----------------         ---------------------
+        
+// Algorithm
+
+// -> Iterate LinkedList from left to right
+// -> Process each accumulative value in this datastructure.       
+//    (unordered_map<int,ListNode*>um) um[ac] = currentNode;
+// -> Given a current accumulative value check if exists in our map (get previous 
+//    element).
+// -> If they exist um[ac]->next = currentNode->next, then delete intermediate  
+//    nodes in our map between um[ac] and currentNode
+        
+        ListNode* root =new ListNode(0);
+        root->next=head;
+        unordered_map<int,ListNode*>um;
+        um[0]=root;
+        int ac=0;
+        
+        while(head!=NULL){
+            ac+=head->val;
+            
+            //found value
+            if(um.find(ac)!=um.end()){
+                ListNode* prev= um[ac];
+                ListNode* start= prev;
+                
+                //delete bad references
+                int aux = ac;
+                while(prev!=head){
+                    prev=prev->next;
+                    aux+=prev->val;
+                    if(prev!=head)um.erase(aux);
                 }
-                head = head->next;
+                
+                start->next = head->next;
+            }else{
+                um[ac]= head;
             }
             
-            //moving on to the next node for performing the above operation 
-            //operation to check if there exists a sum that equals to zero
-            cur = cur->next;
-            if(cur != NULL)
-            {
-                head = cur->next;
-            }
+            head=head->next;
         }
-        return new_head->next; 
+        
+        return root->next;      
+
     }
 };
