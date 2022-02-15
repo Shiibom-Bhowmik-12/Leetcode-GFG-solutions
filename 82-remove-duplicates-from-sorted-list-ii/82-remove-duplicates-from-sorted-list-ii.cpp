c68082e4164f6fd2663a9505867fbe9b0e901ff8
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-02-15 20:50:03 +0530

    Time: 3 ms (97.39%), Space: 11.1 MB (59.53%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,29 +11,51 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL or head->next==NULL){
-            return head;
-        }
         
-        ListNode *curr=head;
-        unordered_map<int,int> mp;
-        while(curr){
-            mp[curr->val]++;
-            curr=curr->next;
-        }
+//----------------  T.C - O(N), S.C - O(N) ----------------------------
+//         if(head==NULL or head->next==NULL){
+//             return head;
+//         }
         
-        ListNode *dummy=new ListNode(0);
-        ListNode *temp=dummy;
-        curr=head;
-        while(curr){
-            if(mp[curr->val]==1){
-                temp->next=curr;
-                temp=temp->next;
+//         ListNode *curr=head;
+//         unordered_map<int,int> mp;
+//         while(curr){
+//             mp[curr->val]++;
+//             curr=curr->next;
+//         }
+        
+//         ListNode *dummy=new ListNode(0);
+//         ListNode *temp=dummy;
+//         curr=head;
+//         while(curr){
+//             if(mp[curr->val]==1){
+//                 temp->next=curr;
+//                 temp=temp->next;
+//             }
+//             curr=curr->next;
+//         }
+        
+//         temp->next=NULL;
+//         return dummy->next;
+//---------------------------------------------------------------------------
+      ListNode *last = new ListNode(0), *new_head = last;
+        
+        while (head) {
+            
+            if (head->next && head->val == head->next->val) {
+                while (head->next && head->val == head->next->val)
+                    head = head->next;
             }
-            curr=curr->next;
+            
+            else {
+                last->next = head;
+                last = head;
+            }
+            
+            head = head->next;
         }
         
-        temp->next=NULL;
-        return dummy->next;
+        last->next = NULL;
+        return new_head->next;
     }
 };
