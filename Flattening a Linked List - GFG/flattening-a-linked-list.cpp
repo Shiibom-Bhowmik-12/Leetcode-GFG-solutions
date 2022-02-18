95ba2a0342d5ff896be2838369c61c6a69c8c5c1
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-02-18 08:00:00 +0530

    Add files via upload

---
+++ Flattening a Linked List - GFG/flattening-a-linked-list.cpp
@@ -0,0 +1,173 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+Node *sortedMerge(Node* head1, Node* head2)  
+{  
+    // code here
+    if(head1==NULL){
+        return head2;
+    }
+    
+    if(head2==NULL){
+        return head1;
+    }
+    
+    Node *start=new Node(-1);
+    Node *end=start;
+    if(head1->data <= head2->data){
+        end->bottom=head1;
+        end=end->bottom;
+        head1=head1->bottom;
+    }
+    else{
+        end->bottom=head2;
+        end=end->bottom;
+        head2=head2->bottom;
+    }
+    
+    while(head1!=NULL and head2!=NULL){
+        if(head1->data <= head2->data){
+            end->bottom=head1;
+            end=end->bottom;
+            head1=head1->bottom;
+        }
+        else{
+            end->bottom=head2;
+            end=end->bottom;
+            head2=head2->bottom;
+        }
+    }
+    
+    if(head1==NULL){
+        end->bottom=head2;
+    }
+    
+    if(head2==NULL){
+        end->bottom=head1;
+    }
+    
+    return start->bottom;
+}  
+
+Node *flatten(Node *root)
+{
+   // Your code here
+   if(root==NULL or root->next==NULL){
+       return root;
+   }
+   
+   root->next=flatten(root->next);
+   root=sortedMerge(root,root->next);
+   
+   return root;
+}
+
