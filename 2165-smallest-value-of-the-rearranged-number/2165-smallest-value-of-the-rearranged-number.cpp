62f053915bb4e48dde3e1c30cd75173b4f4dff00
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-02-27 03:19:43 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (65.23%) - LeetHub

---
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    long long smallestNumber(long long num) {
+        auto s=to_string(num);
+        
+        if(num>0){
+            sort(s.begin(),s.end());
+            if(s[0]=='0'){
+                int i=0;
+                while(s[i]=='0'){
+                    i++;
+                }
+                
+                swap(s[i],s[0]);
+            }
+        }
+        else{           
+            sort(s.begin()+1,s.end(),greater<char>());
+            
+            if(s[1]=='0'){
+            for(char c=1;c<s.size();c++){
+               if(s[c]!=0){
+                   swap(s[1],s[c]);
+               }
+                else{
+                    break;
+                } 
+             }
+           }
+        }
+        
+        return stoll(s);
+    }
+};
