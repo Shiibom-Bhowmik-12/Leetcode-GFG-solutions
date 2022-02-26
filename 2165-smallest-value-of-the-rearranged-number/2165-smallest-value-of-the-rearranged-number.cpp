3b421f4683f8900d53a03d111e70d32be3e73f93
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-02-27 03:44:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (65.23%) - LeetHub

--- 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
+++ 2165-smallest-value-of-the-rearranged-number/2165-smallest-value-of-the-rearranged-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     long long smallestNumber(long long num) {
-        auto s=to_string(num);
+        auto s=to_string(num);//converting it to string for convininence
         
+        //if the number is greater than 0, then we habe to sort the
+        //digits in ascending order.If there are leading zeroes 
+        //then we just have to swap the 0th position zero with the first 
+        //non-zero digit.
         if(num>0){
             sort(s.begin(),s.end());
+            
+            //if there are leading zeroes then we run a loop to look for
+            //non-zero digit.
             if(s[0]=='0'){
                 int i=0;
                 while(s[i]=='0'){
@@ -14,21 +21,12 @@ public:
                 swap(s[i],s[0]);
             }
         }
-        else{           
+        else{  
+            //if the given number is less than 0, then we just sort the
+            //given number in descending order.
             sort(s.begin()+1,s.end(),greater<char>());
-            
-           //  if(s[1]=='0'){
-           //  for(char c=1;c<s.size();c++){
-           //     if(s[c]!=0){
-           //         swap(s[1],s[c]);
-           //     }
-           //      else{
-           //          break;
-           //      } 
-           //   }
-           // }
         }
         
-        return stoll(s);
+        return stoll(s);//returning the resultant from string to long long.
     }
 };
