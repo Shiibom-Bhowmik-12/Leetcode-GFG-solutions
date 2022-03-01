617aacf8e90023d1f7347f4a6c508a88873c6e6f
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-03-01 23:58:48 +0530

    Added solution - LeetHub

--- Concatenate two numbers - GFG/concatenate-two-numbers.cpp
+++ Concatenate two numbers - GFG/concatenate-two-numbers.cpp
@@ -7,26 +7,41 @@ using namespace std;
 //User function Template for C++
 class Solution{
     public:
+
     long long countPairs(int N, int X, vector<int> numbers){
             long long int ans=0;
             unordered_map<string ,int>mp;
             for(auto i:numbers)
             {
-            mp[to_string(i)]++;
+               mp[to_string(i)]++;
             }
             string s=to_string(X);
             for(int i=1;i<s.size();i++)
             {
-            string first=s.substr(0,i);
-            string second=s.substr(i);
-            if(first==second)
-            ans+=mp[first]*(mp[second]-1);
-            else 
-            ans+=mp[first]*mp[second];
+                string first=s.substr(0,i);
+                string second=s.substr(i);
+                if(first==second)
+                    ans+=mp[first]*(mp[second]-1);
+                else 
+                    ans+=mp[first]*mp[second];
             }
             return ans;
     }
 }; 
+
+// 1.first convert all vector element into string using to_string function.
+
+// 2. convert X into string.
+
+// 3. take a loop of size X.
+
+// 4. take two string first and second within loop .
+
+// 5. check substring of X if it exist then multiply the values.
+
+//  such as s="12345"   first=1 and second=2345
+
+// if 1 and 2345 pairs exists in map then multiply how much time 1 occurs and how much time 2345 occurs .
 
 // { Driver Code Starts.
 int main() 
