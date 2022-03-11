287ec29e581c0dc679712c6ebf60cd144d8e112e
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-03-11 06:48:59 +0530

    Added solution - LeetHub

---
+++ Permutation with Spaces - GFG/permutation-with-spaces.cpp
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+public:
+    void solve(string ip,string op,vector<string> &res){
+        if(ip==""){
+            res.emplace_back(op);
+            return;
+        }
+        
+        string op1=op;
+        string op2=op;
+        op1.push_back(ip[0]);
+        op2.push_back(' ');
+        op2.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        
+        solve(ip,op1,res);
+        solve(ip,op2,res);
+    }
+    
+    vector<string> permutation(string S){
+        // Code Here
+        vector<string> res;
+        string op="";
+        op.push_back(S[0]);
+        string ip=S;
+        ip.erase(ip.begin()+0);
+        
+        solve(ip,op,res);
+        sort(res.begin(),res.end());
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int  main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        vector<string> ans;
+        Solution obj;
+        ans = obj.permutation(S);
+        for(int i=0;i<ans.size();i++){
+            cout<<"("<<ans[i]<<")";
+        }
+        cout << endl;
+    }
+}
+  // } Driver Code Ends
