3f86d4f2bac7b4da5c58dba59dd57686df557173
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-07-16 02:07:13 +0530

    Time: 15 ms (7.82%), Space: 12.2 MB (36.60%) - LeetHub

---
+++ 31-next-permutation/31-next-permutation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int size=nums.size(),i,j;
+        for(i=size-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                break;
+            }
+        }
+        
+        if(i<0){
+            reverse(nums.begin(),nums.end());
+        }
+        else{
+            for(j=size-1;j>i;j--){
+                if(nums[j]>nums[i]){
+                    break;
+                }
+            }
+            
+            swap(nums[i],nums[j]);
+            reverse(nums.begin()+i+1,nums.end());
+        }
+        
+        // next_permutation(nums.begin(),nums.end());
+    }
+};
