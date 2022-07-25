fbcb4b03cd0a945c2e3b602596d3a4c943fa300c
Shibom Bhowmik <86771481+Shiibom-Bhowmik-12@users.noreply.github.com>
2022-07-26 03:57:50 +0530

    Time: 35 ms (97.54%), Space: 20.7 MB (12.33%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+                 // Code here
+         vector<pair<int,int>> vp;
+         
+         for(int i=0;i<intervals.size();i++){
+            vp.push_back({intervals[i][0],intervals[i][1]});
+         }
+         
+         sort(vp.begin(),vp.end());
+         
+         int idx=0;
+         for(int i=1;i<vp.size();i++){
+             if(vp[idx].second >= vp[i].first){
+                 vp[idx].second=max(vp[idx].second,vp[i].second);
+                 vp[idx].first=min(vp[idx].first,vp[i].first);
+             }
+             else{
+                 idx++;
+                 vp[idx]=vp[i];
+             }
+         }
+         
+         vector<vector<int>> v;
+         for(int i=0;i<=idx;i++){
+             vector<int> res;
+             res.push_back(vp[i].first);
+             res.push_back(vp[i].second);
+             v.push_back(res);
+         }
+         
+         return v;
+    }
+};
